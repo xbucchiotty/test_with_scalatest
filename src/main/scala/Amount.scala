fb532382def3b3e6e6f7f56c0e7e48f34c1619be
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2013-06-05 08:54:28 +0200

    a solution possibility

--- src/main/scala/Amount.scala
+++ src/main/scala/Amount.scala
@@ -1,3 +1,18 @@
 import java.util.Currency
 
-case class Amount(value: Double, currency: Currency)
+case class Amount(value: Double, currency: Currency) {
+  def add(right: Amount)(implicit exchanges: Exchanges): Amount = if (right.currency == this.currency) {
+    copy(value = value + right.value)
+  } else {
+    this.add(right.changedIn(this.currency))
+  }
+
+  def multipliedBy(coefficient: Double): Amount = copy(value = value * coefficient)
+
+  def +(right: Amount)(implicit exchanges: Exchanges) = add(right)
+
+  def * = multipliedBy(_)
+
+  def changedIn(counterpart: Currency)(implicit exchanges: Exchanges): Amount = exchanges(this, counterpart)
+
+}
--- src/test/scala/AmountTest.scala
+++ src/test/scala/AmountTest.scala
@@ -1,2 +1,92 @@
+import java.util.Currency
+import org.scalatest.FunSpec
+import org.scalatest.matchers.{MatchResult, BeMatcher, ShouldMatchers}
+import AmountImplicits._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
-class AmountTest
+class AmountTest extends FunSpec with ShouldMatchers with AmountImplicits with GeneratorDrivenPropertyChecks {
+
+  describe("An amount") {
+    it("can be added to another amount of the same currency") {
+      implicit val noExchanges = Exchanges.noop
+
+      forAll {
+        (a: Double, b: Double) => {
+          val result = Amount(a, "EUR") + Amount(b, "EUR")
+          result should have('value(a + b))
+          result should be(in("EUR"))
+        }
+      }
+    }
+
+    it("can multiply a coefficient") {
+      implicit val noExchanges = Exchanges.noop
+
+      forAll {
+        (a: Double, b: Double) => {
+          val result = Amount(a, "EUR") * b
+          result should have('value(a * b))
+          result should be(in("EUR"))
+        }
+      }
+
+
+    }
+
+    it("can be multiplied by a coefficient") {
+      forAll {
+        (a: Double, b: Double) => {
+          val result = a * Amount(b, "EUR")
+          result should have('value(a * b))
+          result should be(in("EUR"))
+        }
+      }
+    }
+
+    it("can be changed in a given counterpart currency") {
+      implicit val exchanges = Exchanges(Map(Instrument("EUR", "USD") -> 2))
+
+      forAll {
+        a: Double => {
+          Amount(a, "EUR") changedIn ("USD") should have('value(a * 2))
+          Amount(a, "EUR") changedIn ("USD") should be(in("USD"))
+        }
+      }
+    }
+
+    it("can be added to another amount of different currency given exchanges") {
+      implicit val exchanges = Exchanges(Map(Instrument("EUR", "USD") -> 2))
+
+      forAll {
+        (a: Double, b: Double) => {
+          val left = Amount(a, "EUR")
+          val right = Amount(b, "USD")
+
+          val result = left + right
+          result should be(in("EUR"))
+          result should have('value(a + b / 2))
+        }
+      }
+    }
+  }
+}
+
+trait AmountImplicits {
+  def in(expectation: Currency): BeMatcher[Amount] = new BeMatcher[Amount] {
+    def apply(left: Amount): MatchResult = MatchResult(left.currency == expectation, s"Expected currency $expectation for :$left ", s"Expected currency different from $expectation for :$left ")
+  }
+
+  implicit def stringToCurrency(currencyCode: String): Currency = Currency.getInstance(currencyCode)
+
+}
+
+object AmountImplicits {
+
+  implicit class ExtendedDouble(val coefficient: Double) extends AnyVal {
+    def multiply(right: Amount): Amount = right.multipliedBy(coefficient)
+
+    def *(right: Amount): Amount = right.multipliedBy(coefficient)
+
+  }
+
+}
